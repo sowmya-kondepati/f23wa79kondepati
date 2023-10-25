6a1991fa5c6b4727f5a3f8b21d71f734febd713d
sowmya-kondepati <kondepatisowmya123@gmail.com>
2023-10-25 18:50:41 -0500

    created bonus end point

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var bonusRouter = require('./routes/bonus');
+var computationRouter = require('./routes/computation');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
-app.use('/bonus', bonusRouter);
+app.use('/computation', computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
